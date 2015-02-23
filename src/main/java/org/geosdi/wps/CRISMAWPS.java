578e98d5d1391d031eef6b7c4b19fa3550428bcf
andypower <ssileno@tiscali.it>
2015-02-23 17:00:10 +0100

    Switching to the GWT 2.7 RC1 version and supporting superdevmode
    To run maplite you need to launch the following commands form maplite-client folder:
    mvn gwt:run-codeserver (optional -Dgwt.codeServerPort=1212)
    mvn jetty:run
    
    the application will be available on localhost:9393 i.e.: http://localhost:9393/geoSDIMapLite.html?mapID=650-400&x=14.945351250000678&y=42.64071690252603&zoom=5

---
+++ src/main/java/org/geosdi/wps/CRISMAWPS.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.geosdi.wps;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.geotools.process.factory.DescribeParameter;
+import org.geotools.process.factory.DescribeProcess;
+import org.geotools.process.factory.DescribeResult;
+import org.geoserver.wps.gs.GeoServerProcess;
+
+/**
+ * @author Nazzareno Sileno - CNR IMAA geoSDI Group
+ * @email nazzareno.sileno@geosdi.org
+ */
+public class CRISMAWPS implements GeoServerProcess {
+
+    @DescribeProcess(title = "crismaWPS", description = "CRISMA WPS ICCM aware")
+    @DescribeResult(name = "result", description = "output result")
+    public String execute(@DescribeParameter(name = "name", description = "name to return") String name) {
+        return "Hello, " + name;
+    }
+
+    @DescribeProcess(title = "Hazard Model", description = "CRISMA WPS for Hazard Model elaboration")
+    @DescribeResult(name = "intens grid", description = "WFS link for intensity distribution map")
+    public String hazard(@DescribeParameter(name = "isShakemap", description = "Shakemap presence") boolean isShakeMa,
+            @DescribeParameter(name = "shakeMapName", description = "Shakemap table name") String shakeMapName,
+            @DescribeParameter(name = "latitude", description = "Epi center latitude") Double lat,
+            @DescribeParameter(name = "longitude", description = "Epi center longitude") Double lon,
+            @DescribeParameter(name = "magnitude", description = "Earthquake magnitude") Double mag) throws Exception{
+
+        Connection conn = this.connectToDatabaseOrDie();
+
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM mytable WHERE columnfoo = 500");
+        while (rs.next()) {
+            System.out.print("Column 1 returned ");
+            System.out.println(rs.getString(1));
+        }
+        rs.close();
+        st.close();
+
+        //This example issues the same query as before but uses a PreparedStatement and a bind value in the query.
+
+//        int foovalue = 500;
+//        PreparedStatement st = conn.prepareStatement("SELECT * FROM mytable WHERE columnfoo = ?");
+//        st.setInt(1, foovalue);
+//        ResultSet rs = st.executeQuery();
+//        while (rs.next()) {
+//            System.out.print("Column 1 returned ");
+//            System.out.println(rs.getString(1));
+//        }
+//        rs.close();
+//        st.close();
+
+        return "Ekkolo";
+    }
+
+    private Connection connectToDatabaseOrDie() {
+        Connection conn = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://localhost/crisma";
+            conn = DriverManager.getConnection(url, "postgres", "0x,postgres,0x");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(2);
+        }
+        return conn;
+    }
+
+}
